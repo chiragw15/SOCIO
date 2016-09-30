570a462e713587bda357c6557659b8767c8f0ba9
cc15 <chiragwadhera15@gmail.com>
2016-10-01 01:12:54 +0530

    Adding upload activity

---
+++ app/src/main/java/com/example/cc15/soco/FragmentUpload2.java
@@ -0,0 +1,22 @@
+package com.example.cc15.soco;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by cc15 on 30/9/16.
+ */
+public class FragmentUpload2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.upload2_fragment, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/cc15/soco/MainActivity.java
+++ app/src/main/java/com/example/cc15/soco/MainActivity.java
@@ -8,8 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentUpload1 fragmentUpload1 = new FragmentUpload1();
+    FragmentUpload2 fragmentUpload2 = new FragmentUpload2();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentUpload1, "Tag").addToBackStack(null).commit();
+        }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
