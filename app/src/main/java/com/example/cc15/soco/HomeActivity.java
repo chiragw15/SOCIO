57bbf6f8c3772d2a02ebd706373f2210511ea82e
cc15 <chiragwadhera15@gmail.com>
2016-10-01 03:55:20 +0530

    Adding Home Activity

--- app/src/main/java/com/example/cc15/soco/HomeActivity.java
+++ app/src/main/java/com/example/cc15/soco/HomeActivity.java
@@ -3,11 +3,16 @@ package com.example.cc15.soco;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    //private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,10 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
     }
 
 }
+
