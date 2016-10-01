14cc2299dee62b3b7cec9d5970385e578a3c206e
ArinjayaKhare1 <arinjayakhare1@gmail.com>
2016-10-01 15:11:36 +0530

    Added add activity and logo activity

--- app/src/main/java/com/example/cc15/soco/postsFragment.java
+++ app/src/main/java/com/example/cc15/soco/postsFragment.java
@@ -2,6 +2,7 @@ package com.example.cc15.soco;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,8 @@ public class postsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_posts, container, false);
+
+
 
     }
 
