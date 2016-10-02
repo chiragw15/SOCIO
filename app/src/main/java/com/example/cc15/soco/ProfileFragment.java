b6a01745d29214a433a349fd5b28bfdc5f4a2411
ArinjayaKhare1 <arinjayakhare1@gmail.com>
2016-10-02 09:24:26 +0530

    Adding Profile Fragment

---
+++ app/src/main/java/com/example/cc15/soco/ProfileFragment.java
@@ -0,0 +1,35 @@
+package com.example.cc15.soco;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ArinjayaKhare1 on 02-10-2016.
+ */
+
+public class ProfileFragment extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.profile_layout, container, false);
+        //ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        //SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getActivity(),getChildFragmentManager());
+
+        // Set the adapter onto the view pager
+        //viewPager.setAdapter(adapter);
+        //TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        //tabLayout.setupWithViewPager(viewPager);
+        return view;
+    }
+
+
+}
